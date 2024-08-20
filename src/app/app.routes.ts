e5c2f1a37e7f39facefc1652c57c5e1bcca1b139
Bryan Reamillo <bryanreamillo2403@gmail.com>
2024-08-21 00:07:04 +0800

    added components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { CreateComponent } from './create/create.component';
+import { EditComponent } from './edit/edit.component';
+import { DeleteComponent } from './delete/delete.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: AppComponent},
+    {path: 'create', component: CreateComponent},
+    {path: 'edit', component: EditComponent},
+    {path: 'delete', component: DeleteComponent},
+ 
+];
