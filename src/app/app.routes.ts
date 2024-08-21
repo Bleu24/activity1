f2a4e8f9877f778f6768bdbbf555705245ef5138
Bryan Reamillo <bryanreamillo2403@gmail.com>
2024-08-21 23:57:50 +0800

    second init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { CreateComponent } from './create/create.component';
 import { EditComponent } from './edit/edit.component';
 import { DeleteComponent } from './delete/delete.component';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    {path: '', component: AppComponent},
+    {path: '', component: HomeComponent},
+    {path: 'home', component: HomeComponent},
     {path: 'create', component: CreateComponent},
     {path: 'edit', component: EditComponent},
     {path: 'delete', component: DeleteComponent},
